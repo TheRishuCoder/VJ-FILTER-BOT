5936b63022848b48658b186b76bf09bc1c98e1c4
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-11-14 12:57:01 +0530

    Update repo.py

--- plugins/Extra/repo.py
+++ plugins/Extra/repo.py
@@ -35,7 +35,7 @@ async def git(bot, message):
 
 <b>Open Issues :</b> <i>{qw.get("open_issues")}</i>
 
-<b>Powered by :</b> @VJ_Botz
+<b>Powered by :</b> @Ur_rishu_143
 
 """
         if qw.get("description"):
